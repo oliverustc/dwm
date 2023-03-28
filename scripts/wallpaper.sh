f4b36c2a3a51afbd3609582cbee3b8e278f8a5de
oliver <oliverustc@hotmail.com>
2023-03-28 09:25:52 +0800

    20230328 custom

--- scripts/wallpaper.sh
+++ scripts/wallpaper.sh
@@ -6,4 +6,4 @@ wallpapers=$(find $MWPP | tail -n +2 | shuf | head -n 2 )
 virtical_wallpaper=$(find $VWPP | tail -n +2 | shuf | head -n 1)
 echo $wallpapers > /tmp/wallpaper.log
 echo $virtical_wallpaper >> /tmp/wallpaper.log
-feh --bg-scale $wallpapers $virtical_wallpaper
+feh --bg-fill $wallpapers $virtical_wallpaper
